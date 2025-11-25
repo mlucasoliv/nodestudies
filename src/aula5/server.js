6af12a3eb125e5c9e813072e87c49b7faec07df4
Marcelo <marcelolucasdeoliveiraa@gmail.com>
2025-11-25 12:43:47 -0300

    saídas

--- src/aula5/server.js
+++ src/aula5/server.js
@@ -2,6 +2,10 @@ const app = require('./app')
 const PORT = process.env.PORT || 5000;
 const path = require('path')
 
+app.use(express.urlencoded({ extended: true}))
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
